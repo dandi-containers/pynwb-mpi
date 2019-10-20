e2e4127767826f209503814e3e996a2709f56770
Yaroslav Halchenko <debian@onerussian.com>
2019-10-20 17:45:15 -0400

    neurodocker script to generate both docker and singularity files

---
+++ generate_container.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -eu
+
+generate() {
+  #docker run --rm kaczmarj/neurodocker generate "$1" \
+  neurodocker generate "$1" \
+    --base=debian:testing \
+    --pkg-manager=apt \
+    --install python3-h5py python3-pip hdf5-tools \
+    --run "mkdir -p /opt/mpidemos" \
+    --copy test2.py /opt/mpidemos \
+    --run "chmod a+rX -R /opt/mpidemos" \
+    --run "python3 -m pip install pynwb" \
+    --user=nwb
+}
+
+generate docker > Dockerfile
+generate singularity > Singularity
+
